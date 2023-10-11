08c6083a646e465804c345ac18969c094dc423db
yucf7 <frikhatyoussef@gmail.com>
2023-10-11 14:53:04 +0200

    add order confirmation

--- client/src/components/Delivery/delivery-component.tsx
+++ client/src/components/Delivery/delivery-component.tsx
@@ -1,13 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './delivery-component.css';
-import { useNavigate } from 'react-router-dom';
 
 function DeliveryComponent() {
-  const navigate = useNavigate();
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
   const [address, setAddress] = useState("");
-  const [zipcode, setZipcode] = useState("");
   const [alertMessage, setAlertMessage] = useState(""); 
   const [cartItems, setCartItems] = useState<any[]>([]);
   const [total, setTotal] = useState(0);
@@ -36,8 +31,6 @@ function DeliveryComponent() {
       let res = await fetch("http://localhost:4000/order", {
         method: "POST",
         body: JSON.stringify({
-          firstName: firstName,
-          lastName: lastName,
           address: address,
           total: total,
           user: user,
@@ -70,37 +63,10 @@ function DeliveryComponent() {
         <div className="form-group">
           <input
             type="text"
-            id="firstName"
-            placeholder="First Name"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-            required />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            id="lastName"
-            placeholder="Last Name"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-            required />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
             id="text"
             placeholder="Address"
             value={address}
             onChange={(e) => setAddress(e.target.value)}
-            required />
-        </div>
-        <div className="form-group">
-          <input
-            type="text"
-            id="text"
-            placeholder="Zip Code"
-            value={zipcode}
-            onChange={(e) => setZipcode(e.target.value)}
             required />
         </div>
         <button type="submit">Confirm</button>
