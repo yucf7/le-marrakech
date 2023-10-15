a7714a383a27aac2cf7fd09f0cca1a93c37a7789
yucf7 <frikhatyoussef@gmail.com>
2023-10-15 22:27:03 +0200

    add docker compose yml

--- server/db/db_init.js
+++ server/db/db_init.js
@@ -5,17 +5,28 @@ const path = require('path');
 
 const jsonFilePath = path.join(__dirname, 'meals.json');
 
+const {
+    DB_HOST,
+    DB_PORT,
+    DB_NAME,
+  } = process.env;
+  
+const dbURL=`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`
+
 module.exports.connect = async () =>{
-        mongoose.connect('mongodb://127.0.0.1:27017/le_marrakech',{})
+    mongoose.connect(dbURL, {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        family: 4,
+    })
         .then((result) =>{
             console.log('connected to mongo')
         }) 
         .catch((err) =>{
-            console.error(err);
+            console.error('MongoDB connection error:',err);
     })
     
 }
-
 // this code to fill db with meals
 module.exports.fillDataBase = async ()=>{
     try {
